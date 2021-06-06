d655de6688372393aa4436a103cecd7241e31a0b
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-06 16:59:03 +0000

    Adding def delete and updating all in file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+classes = {
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+            }
 
 
 class FileStorage:
@@ -8,9 +22,28 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def delete(self, obj=None):
+        """Deletes an object from __objects if it exists"""
+
+        if obj:
+            key = type(obj).__name__ + "." + obj.id
+            del FileStorage.__objects[key]
+            del obj
+
+
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if not cls:
+            return FileStorage.__objects
+        else:
+            tmp_dic = {}
+            tmp_list = str(cls).split(".")
+            cls_name = tmp_list[2]
+            cls_name = cls_name[:-2]
+            for key in FileStorage.__objects:
+                if cls_name in key:
+                    tmp_dic[key] = FileStorage.__objects[key]
+            return tmp_dic
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -27,19 +60,7 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
 
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
