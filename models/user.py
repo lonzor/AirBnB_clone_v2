05cca365df980b947e6685cb0ad170797f8f3598
Lonzo Rust <60586779+lonzor@users.noreply.github.com>
2021-06-08 20:01:28 -0500

    Revert "Adding db attributes to remaining classes except amenity"

--- models/user.py
+++ models/user.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete-orphan")
-    reviews = relationship("Review", backref="user", cascade="all, delete-orphan")
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
