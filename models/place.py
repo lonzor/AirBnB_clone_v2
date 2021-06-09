60948ebbe8d00dd88783a6667951bf4dd4b45578
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-06-09 01:30:35 +0000

    Reverted a bit too far.

--- models/place.py
+++ models/place.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.review import Review
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
     amenity_ids = []
+
+    @property
+    def reviews(self):
+        review_list = []
+        tmp_dic = models.storage.all(Review)
+        for key in tmp_dic:
+            if tmp_dic[key].place_id == self.id:
+                review_list.append(tmp_dic[key])
+        return review_list
