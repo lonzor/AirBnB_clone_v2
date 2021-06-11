b5b034393bd6437a5dfdf8a1d7952110c1da2672
Jay Calhoun <jwcalhoun2@gmail.com>
2021-06-11 22:25:12 +0000

    fixing pep

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,6 +9,7 @@ import os
 
 env.hosts = ['3.84.179.220', '54.226.50.222']
 
+
 def do_deploy(archive_path):
     """ Deploy an archive """
 
@@ -20,13 +21,17 @@ def do_deploy(archive_path):
 
         put(arhive_path, '/tmp/' + archiveName)
         run("sudo mkdir -p /data/web_static/releases/" + archNameNoExt)
-        run("sudo tar -xzvf /tmp/" + archiveName + " -C " + "/data/web_static/releases/" + archNameNoExt + " --strip-components=1")
+        run("sudo tar -xzvf /tmp/" + archiveName + " -C " +
+            "/data/web_static/releases/" + archNameNoExt +
+            " --strip-components=1")
         run("sudo rm -f /tmp/" + archiveName)
         run("sudo rm -f /data/web_static/current")
-        run("sudo ln -sf /data/web_static/releases/" + archNameNoExt + "/data/web_static/current")
+        run("sudo ln -sf /data/web_static/releases/" +
+            archNameNoExt + "/data/web_static/current")
         return True
     except:
         return False
+
 
 def do_pack():
     """ Pack up our web_static """
