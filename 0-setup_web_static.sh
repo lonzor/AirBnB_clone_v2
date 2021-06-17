1f39d316bf45967cd854b940f4e510921e0b9b25
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-06-17 16:30:19 +0000

    Adding file for task 0.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
-# setup server for web static
+# preparing server for deployment
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
+
 sudo apt-get -y install nginx
-htmlvar="<html>\n <head>\n </head>\n <body>\n\tHolberton School\n </body>\n</html>"
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo -e "$htmlvar" | sudo tee /data/web_static/releases/test/index.html
+sudo service nginx start
+
+# maybe don't want or need sudo
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo echo "Howdy" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "s@^\tlocation / {@\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n\n\tlocation / {@" /etc/nginx/sites-available/default
+sudo chown -hR ubuntu:ubuntu /data
+
+# edit nginx cnfig file
+find="^\tlocation / {"
+replace="\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\n\tlocation / {"
+
+sudo sed -i "s@${find}@${replace}@" /etc/nginx/sites-available/default
+
 sudo service nginx restart
 exit 0
