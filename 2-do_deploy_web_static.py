c8d833684a2fd9de1017b685dec7cdecf11c0d3c
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-06-17 16:57:39 +0000

    Changes to file.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,44 +7,39 @@ from datetime import datetime
 import os
 
 
-env.hosts = ['3.84.179.220', '54.226.50.222']
+env.hosts = ['35.237.103.166', '3.88.182.73']
 
 
-def do_deploy(archive_path):
-    """ Deploy an archive """
+def do_deploy():
+    """ Deploys the archive """
 
     if not os.path.exists(archive_path):
-            return False
+        return False
     try:
-        archiveName = archive_path[9:]
-        archNameNoExt = archiveName[:-4]
-        tarcmd = "sudo tar -xzvf /tmp/" + archiveName + " -C "
-        datarel = "/data/web_static/releases/"
-        datacur = "/data/web_static/current"
-
-        put(archive_path, '/tmp/' + archiveName)
-        run("sudo mkdir -p " + datarel + archNameNoExt)
-        run(tarcmd + datarel + archNameNoExt + " --strip-components=1")
-        run("sudo rm -f /tmp/" + archiveName)
-        run("sudo rm -f " + datacur)
-        run("sudo ln -sf " + datarel + archNameNoExt + " " + datacur)
+        arch_name = archive_path[9:]
+        name_no_ext = arch_name[:-4]
+        tar_cmnd = "sudo tar -xzvf /tmp/" + arch_name + " -C "
+        rel_dir = "/data/web_static/releases/"
+        cur_dir = "/data/web_static/current/"
+        put(archive_path, '/tmp/' + arch_name)
+        run("sudo mkdir -p " + rel_dir + name_no_ext)
+        run(tar_cmnd + rel_dir + name_no_ext + " --strip-components=1")
+        run("sudo rm -f /tmp/" + arch_name)
+        run("sudo rm -f " + cur_dir)
+        run("sudo ln -sf " + rel_dir + name_no_ext + " " + cur_dir)
         return True
     except:
         return False
 
 
 def do_pack():
-    """ Pack up our web_static """
-
+    """packs up all files web_static"""
     try:
         now = datetime.now()
-
-        tarArchiveName = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
-        tarArchivePath = "versions/" + tarArchiveName
-
+        ta_name = "web_static_" + now.strftime("%Y%m%d%H%M%S") + ".tgz"
+        ta_path = "versions/" + ta_name
         local("mkdir -p versions")
-        local("tar -czvf " + tarArchivePath + " web_static")
-
-        return tarArchivePath
+        local("tar -czvf " + ta_path + " web_static")
+        return ta_path
     except:
         return None
