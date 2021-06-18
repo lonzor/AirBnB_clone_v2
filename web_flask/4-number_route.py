aacf2c8630c620d99215315ef0ca957b57105178
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-06-18 23:25:36 +0000

    Adding file for task 4.

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def display_prompt():
+    """Displays prompt Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display_prompt2():
+    """Displays prompt HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def display_prompt3(text):
+    """Displays prompt C <text>"""
+    return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python/<string:text>')
+@app.route('/python/')
+def display_prompt4(text='is cool'):
+    """Displays Python praising text"""
+    return 'Python %s' % text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def is_a_num(n):
+    """Displays if n is a number or not"""
+    return '%i is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
