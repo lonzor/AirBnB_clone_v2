1e218f42de1ff15b716dc9f1e236126b49ae9aa7
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-06-18 01:09:15 +0000

    Adding py file for task 0.

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/user/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def display_prompt():
+    """Displays prompt Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
