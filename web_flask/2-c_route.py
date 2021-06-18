b63fb3e50ccb2821642cf43bf1fae6696268bc93
rustlonzo@gmail.com <rustlonzo@gmail.com>
2021-06-18 01:41:05 +0000

    Adding file for task 2.

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def display_prompt():
+    """Displays prompt Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def display_prompt2():
+    """Displays prompt HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def display_prompt3(text):
+    """Displays prompt C <text>"""
+    return 'C %s' % text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
